1113a22bf5f4caf999c3fda46bed15433c510e75
Nicolas <nicomar9928@gmail.com>
2026-01-12 23:45:44 -0500

    Fix-password-hash

--- backend/src/data/users.js
+++ backend/src/data/users.js
@@ -3,7 +3,7 @@ const users = [
     id: '1',
     name: 'Test User',
     email: 'test@example.com',
-    password: '$2a$10$oKrOpaJh8MgjYFfiBgIF2O0V0ZzLdCDY6n9L35cFVihOZdWDjat4u' // Password123!
+    password: '$2a$10$ut4czCHgzTUz7WRPwrvm0uDW9Fq//tzpVo1MmnVjjXMbQj5OGmCPG' // Password123!
   }
 ];
 
